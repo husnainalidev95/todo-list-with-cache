6c157844a09ab39cc2694b7cad0f9f94b97657ca
Husnain Ali <83615100+husnainalidev95@users.noreply.github.com>
2022-01-25 21:00:17 +0500

    Revert "Update App.js"
    This reverts commit 401f802c9060bf4ed105550f5ca3a1c65afccfc2.

--- src/App.js
+++ src/App.js
@@ -1,62 +1,24 @@
-import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [cacheData, setCacheData] = React.useState();
-
-  // Function to get all cache data
-  const getAllCacheData = async () => {
-    var url = 'https://localhost:3000';
-
-    // List of all caches present in browser
-    var names = await caches.keys();
-
-    var cacheDataArray = [];
-
-    // Iterating over the list of caches
-    names.forEach(async (name) => {
-
-      // Opening that particular cache
-      const cacheStorage = await caches.open(name);
-
-      // Fetching that particular cache data
-      const cachedResponse = await cacheStorage.match(url);
-      var data = await cachedResponse.json();
-
-      // Pushing fetched data into our cacheDataArray
-      cacheDataArray.push(data);
-      setCacheData(cacheDataArray.join(', '));
-    })
-  };
-
-  const addDataIntoCache = (cacheName, url, response) => {
-    // Converting our response into Actual Response form
-    const data = new Response(JSON.stringify(response));
-
-    if ('caches' in window) {
-      // Opening given cache and putting our data into it
-      caches.open(cacheName).then((cache) => {
-        cache.put(url, data);
-        alert('Data Added into cache!')
-      });
-    }
-  };
-
   return (
-    <>
-      <div style={{ height: 500, width: '80%' }}>
-        <h4>How to get all cache data in ReactJS?</h4>
-        <button onClick={() => getAllCacheData()} >
-          Get All Cache Data</button>  <br />
-        <h6>All Cache Data is: {cacheData}</h6>
-      </div>
-
-      <div style={{ height: 500, width: '80%' }}>
-        <h4>How to store data into cache in ReactJS?</h4>
-        <button onClick={() => addDataIntoCache('MyCache',
-          'https://localhost:300', 'SampleData')} >
-          Add Data Into Cache</button>
-      </div>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
